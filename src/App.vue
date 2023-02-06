96c7f26ccbe27efc73263292c4f211de08a65f96
bricemikembo <MIKbri2000>
2023-02-06 14:55:50 +0100

    export front bimiot

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import ModelViewer from './components/ModelViewer.vue'
+
+export default {
+    name: 'app',
+    components: {
+        ModelViewer
+    },
+    created() {
+        document.title = 'Vue IFC.js Example'
+    }
+}
+</script>
+
+<template>
+  <div id="app">
+    <ModelViewer />
+  </div>
+</template>
+
+<style>
+
+html {
+    overflow-y: hidden !important;
+}
+
+#app {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    height: 100%;
+}
+
+</style>
